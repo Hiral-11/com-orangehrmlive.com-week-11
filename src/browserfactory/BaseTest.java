8cdb5867b757ffc7cc7761e24056480bd4df1788
Hiral-11 <hiral.karelia1@gmail.com>
2023-11-21 18:14:49 +0000

    week 11 orangehrmlive HK

---
+++ src/browserfactory/BaseTest.java
@@ -0,0 +1,46 @@
+package browserfactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+/**
+ * . Create the package ‘browserfactory’ and create the
+ * class with the name ‘BaseTest’ inside the
+ * ‘browserfactory’ package. And write the browser setup
+ * code inside the class ‘Base Test’.
+ */
+
+public class BaseTest {
+
+    static String browser = "Chrome";
+
+    static String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+
+    public static WebDriver driver; // declaring the variable outside main method
+
+    public void openBrowser(String baseUrl) { //call this method for base url
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Wrong Browser name");
+        }
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public void closerBrowser() {
+
+        driver.quit();
+    }
+}
+
+
